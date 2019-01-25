c92bd420c804b5a8415be1802fe951d90efac228
congbing <congbing@everybodycreate.com>
2019-01-25 17:44:22 +0800

    提取统一的domain、dao、mapper.xml

---
+++ src/main/java/com/cb/itoken/common/mapper/TbPostsPostMapper.java
@@ -0,0 +1,7 @@
+package com.cb.itoken.common.mapper;
+
+import com.cb.itoken.common.domain.TbPostsPost;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbPostsPostMapper extends MyMapper<TbPostsPost> {
+}
---
+++ src/main/java/com/cb/itoken/common/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.cb.itoken.common.mapper;
+
+import com.cb.itoken.common.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
