d9a2fa32f5681c07c1da5cc6a0e871ca65f3cdad
congbing <congbing@everybodycreate.com>
2019-01-28 01:48:21 +0800

    文章服务消费者页面

--- src/main/java/com/cb/itoken/common/mapper/TbSysUserMapper.java
+++ src/main/java/com/cb/itoken/common/mapper/TbSysUserMapper.java
@@ -1,7 +1,10 @@
 package com.cb.itoken.common.mapper;
 
 import com.cb.itoken.common.domain.TbSysUser;
+import com.cb.itoken.common.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import tk.mybatis.mapper.MyMapper;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbSysUserMapper extends MyMapper<TbSysUser> {
 }
